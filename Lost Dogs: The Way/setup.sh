aa74ce0e93a79b43ba1ce74bc74deccea40e3407
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-08 14:19:51 +0700

    ended season2

--- Lost Dogs: The Way/setup.sh
+++ Lost Dogs: The Way/setup.sh
@@ -30,16 +30,15 @@ fi
 create_default_configs() {
     cat > configs.json << EOL
 {
-    "timeZone": "en-US",
-    "rotateProxy": false,
-    "skipInvalidProxy": false,
-    "proxyRotationInterval": 2,
-    "delayEachAccount": [5, 8],
-    "timeToRestartAllAccounts": 300,
-    "howManyAccountsRunInOneTime": 10,
-    "doTasks": true,
-    "playGames": true,
-    "referralCode": ""
+  "timeZone": "en-US",
+  "rotateProxy": false,
+  "skipInvalidProxy": false,
+  "proxyRotationInterval": 2,
+  "delayEachAccount": [5, 8],
+  "timeToRestartAllAccounts": 86400,
+  "howManyAccountsRunInOneTime": 100,
+  "doTasks": true,
+  "todayVote": "1"
 }
 EOL
 }
@@ -55,7 +54,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Lost Dogs: The Way BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -115,7 +114,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            node meomundep
             read -p "Press Enter to continue..."
             ;;
         4)
